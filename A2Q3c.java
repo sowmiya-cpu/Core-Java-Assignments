23322462134587392bef3f4d9155166c4094e9f4
sowmiya-cpu <101458076+sowmiya-cpu@users.noreply.github.com>
2022-03-13 09:20:45 +0530

    ASSIGNMENTS 1 & 2 & 3

---
+++ A2Q3c.java
@@ -0,0 +1,14 @@
+package Assignment2Q3;
+
+public class c {
+	public static void main(String[] args) {
+		
+		Account account=new
+				SavingsAccount("ravi", "4343433434", 30000, 5, 1000);
+	
+		
+		Account account2=new 
+			CurrentAccount("rajiv", "543545445", 500000, "AB1234", 50000);
+		
+	}
+}
