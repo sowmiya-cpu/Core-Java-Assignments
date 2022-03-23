8a31da3deeff28206b8928ebefd6af45e1c3f535
sowmiya-cpu <101458076+sowmiya-cpu@users.noreply.github.com>
2022-03-23 14:50:01 +0530

    Add files via upload

---
+++ A4Q1 Frequency main.java
@@ -0,0 +1,12 @@
+package A4Q1;
+
+public class Frequency Main
+{
+   static PrintFrequency f;
+
+	public static void main(String[] args) {
+	
+        System.out.println("The frequency of each word in the file");		
+		f=new PrintFrequency();
+}
+}
